50711cca2283d992ac01b74c6bc1d68e04128861
suwan <blacksw1102@gmail.com>
2022-01-10 13:48:52 +0900

    FaceDetector 모듈화

--- example1.py
+++
@@ -1,8 +0,0 @@
-import face_recognition
-
-image = face_recognition.load_image_file("./image/Elon Musk.jpg")
-face_locations = face_recognition.face_locations(image)
-
-print('location : ', face_locations)
-
-# face_locations is now an array listing the co-ordinates of each face!
--- main.py
+++ main.py
@@ -1,23 +1,83 @@
 import cv2
-import face_detector as fd
+from face_detector import FaceDetector
 
-# 0번 웹캠 인스턴스 바인딩
-video_capture = cv2.VideoCapture(0)
+# 앱단에서 소유하는 유저 데이터
+users = [
+    {
+        "user_id" : 1,
+        "name" : "Elon Musk",
+        "face_feature" : FaceDetector.getFacefeature(".\image\Elon Musk.jpg")
+    },
+    {
+        "user_id" : 2,
+        "name" : "Suwan",
+        "face_feature" : FaceDetector.getFacefeature(".\image\Suwan.jpg")
+    },
+    {
+        "user_id" : 3,
+        "name" : "Jeff Bezos",
+        "face_feature" : FaceDetector.getFacefeature(".\image\Jeff Bezos.jpg")
+    },
+    {
+        "user_id" : 4,
+        "name" : "Messi",
+        "face_feature" : FaceDetector.getFacefeature(".\image\Messi.jpg")
+    }
+]
 
-while True:
-    # frame 추출
-    ret, frame = video_capture.read()
+# 얼굴 인식 정보를 프레임에 드로잉
+def drawFaceInfoInFrame(frame, detected_face_info):
 
-    # 이미지 가공
-    processedFrame = fd.getFrame(frame)
+    # 얼굴 측정 위치 기준 rectangle 색칠
+    for face_info in detected_face_info:
 
-    # 이미지 출력
-    cv2.imshow('Video', frame)
+        # 파라미터 초기화
+        name = face_info['name']
+        top, right, bottom, left = face_info['location']
 
-    # 'q' 키 누르면 프로그램 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # 박스 드로잉
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-# 웹캠 릴리즈
-video_capture.release()
-cv2.destroyAllWindows()
+        # 이름 드로잉
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+# 유저 정보(List<Map>)에서 특정 Key의 Value들을 List로 리턴
+def getValuesInUsers(users, key):
+    names = []
+    for user in users:
+        names.append(user[key])
+    return names
+
+def main():
+
+    # 웹캠 인스턴스 바인딩
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # 프레임 추출                                                                 # 앱 영역
+        ret, frame = video_capture.read()
+
+        # known 유저정보에서 이름, 인코딩값 추출
+        known_face_names = getValuesInUsers(users, "name")
+        known_face_encodings = getValuesInUsers(users, "face_feature")
+
+        # 인식된 얼굴 정보 추출                                                        # 서드파티 영역
+        detected_face_info = FaceDetector.getDetectedFaceInfoInFrame(frame, known_face_names, known_face_encodings, 2)
+
+        # 프레임 드로잉
+        drawFaceInfoInFrame(frame, detected_face_info)
+
+        # 프레임 출력                                                                 # 앱 영역
+        cv2.imshow('Video', frame)
+
+        # 'q' 키 누르면 프로그램 종료                                                  # 앱 영역
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # 웹캠 릴리즈                                                                     # 앱 영역
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+main()
